8030ce6b1bfc02f09b4d052d691ac36aa6dbf860
atreyu23dll <kevinjosmar232317@gmail.com>
2025-09-21 21:03:53 -0600

    Desarrollo del heaeder

--- paginaDeCatalogo/app.py
+++ paginaDeCatalogo/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/UnicTime")
 def home():
-    return "Hola, Flask ya está corriendo!"
+    return render_template('index.html', title="home")
 
 if __name__ == "__main__":
     app.run(debug=True)
